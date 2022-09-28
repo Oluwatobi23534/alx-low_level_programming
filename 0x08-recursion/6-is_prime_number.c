dc9619a908c2e7cd01f43c2dadd677d4496f9a77
Your Name <you@example.com>
2022-09-28 04:00:10 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,34 +1,32 @@
 #include "main.h"
-
+int calculo_primo(int num_, int inf);
 /**
- * is_prime - detects if an input number is a prime number.
- * @n: input number.
- * @c: iterator.
- * Return: 1 if n is a prime number. 0 if n is not a prime number.
- */
-int is_prime(unsigned int n, unsigned int c)
-{
-	if (n % c == 0)
-	{
-		if (n == c)
-			return (1);
-		else
-			return (0);
-	}
-	return (0 + is_prime(n, c + 1));
-}
-/**
- * is_prime_number - detects if an input number is a prime number.
- * @n: input number.
- * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ * is_prime_number - checking for prime number
+ * @n: variable to hold random num
+ * Return: return 1 if prime and 0 if not
  */
 int is_prime_number(int n)
 {
-	if (n == 0)
+	int a = 2;
+
+	if (n == 1)
 		return (0);
 	if (n < 0)
 		return (0);
-	if (n == 1)
+	return (calculo_primo(n, a));
+}
+/**
+ * calculo_primo - function that returns 1 if the input integer is a prime.
+ *@num_: first value
+ *@inf: second value
+ *
+ * Return: Return solution
+ */
+int calculo_primo(int num_, int inf)
+{
+	if (num_ / 2 < inf)
+		return (1);
+	if (num_ % inf == 0)
 		return (0);
-	return (is_prime(n, 2));
+	return (calculo_primo(num_, inf + 1));
 }
