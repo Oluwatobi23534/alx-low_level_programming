774cb7c02cb50312b990a8c47ccb26ab0181cf6b
Your Name <you@example.com>
2022-10-12 05:05:06 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,20 +1,17 @@
+#include <stdio.h>
 #include "3-calc.h"
-#include <stddef.h>
 
 /**
- * get_op_func - gets ops function pointer of type char array
- * that accepts two inputs of int data type
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
  *
- * @s: a character pointer pointing to a symbol from the program argument
- * 
- * Return: one of the operator functions to perform calculations
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
-
- int (*get_op_func(char *s))(int, int)
- {
- 	/* struct opts of struct op_t */
-	op_t opts[] = 
-	{
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -22,15 +19,16 @@
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (i < 5)
+	i = 0;
+
+	while (ops[i].op)
 	{
-		if (*s == *ops[i].op)
-				return (ops[i].f);
-
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
 	}
 
 	return (NULL);
- }
+}
